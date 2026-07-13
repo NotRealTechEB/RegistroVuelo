896ec990a9154eed87c7d2dc92767d0fdfa462c9
Esalamancaa <esteban.salamanca.aravena@gmail.com>
2026-07-12 21:04:00 -0400

    Eliminacion ruta codVuelo en metodo POST

--- src/main/java/cl/dgac/registro/controller/RVController.java
+++ src/main/java/cl/dgac/registro/controller/RVController.java
@@ -127,7 +127,7 @@ public class RVController {
         ) 
     )
     @PostMapping
-    public ResponseEntity<RegistroVuelo> crearRegistro(@RequestParam("codVuelo") String codVuelo, @RequestBody RegistroVuelo nuevoRegistro) {
+    public ResponseEntity<RegistroVuelo> crearRegistro(@RequestBody RegistroVuelo nuevoRegistro) {
         RegistroVuelo registroGuardado = rVService.guardarRegistro(nuevoRegistro);
         return ResponseEntity.status(HttpStatus.CREATED).body(registroGuardado);
     }
